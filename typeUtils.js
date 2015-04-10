7913fc386e363a1386844b233bfe499da17cecfe
Tyler Olson <tydotg@gmail.com>
2015-04-10 13:28:17 -0700

    Use function names

--- typeUtils.js
+++ typeUtils.js
@@ -1,14 +1,6 @@
 var R = require('ramda');
 
-var typeToString = R.cond(
-  [R.eq(String), R.always('String')],
-  [R.eq(Number), R.always('Number')],
-  [R.T,          throwUnkownTypeError]
-);
-
-function throwUnkownTypeError(type) {
-  throw new Error('Unknown data type ' + type);
-}
+var typeToString = R.prop('name');
 
 module.exports = {
   typeToString: typeToString
